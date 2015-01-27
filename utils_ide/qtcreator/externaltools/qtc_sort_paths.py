367f17019ea2e77b5a881596077acc86b4db28b4
Campbell Barton <ideasman42@gmail.com>
2015-01-27 17:26:02 +1100

    cleanup

--- utils_ide/qtcreator/externaltools/qtc_sort_paths.py
+++ utils_ide/qtcreator/externaltools/qtc_sort_paths.py
@@ -2,8 +2,8 @@
 import sys
 
 txt = sys.stdin.read()
-
 data = txt.split("\n")
+
 
 class PathCMP:
     def __init__(self, path):
@@ -24,11 +24,13 @@ class PathCMP:
 
     def __eq__(self, other):
         return self.path == other.path
+
     def __lt__(self, other):
         return self.path < other.path if self.level == other.level else self.level < other.level
+
     def __gt__(self, other):
         return self.path > other.path if self.level == other.level else self.level > other.level
-        
+
 
 data.sort(key=lambda a: PathCMP(a))
 
