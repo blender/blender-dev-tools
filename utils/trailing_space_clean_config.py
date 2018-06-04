fca325137b6ee2dfd0930ca87684ccf30703554f
Campbell Barton <ideasman42@gmail.com>
2018-06-04 18:51:46 +0200

    trailing_space_clean: update config

--- utils/trailing_space_clean_config.py
+++ utils/trailing_space_clean_config.py
@@ -1,6 +1,7 @@
 
 import os
 PATHS = (
+    "intern/ghost",
     "release/scripts/modules",
     "release/scripts/startup",
     "source/blender/bmesh",
@@ -8,9 +9,13 @@ PATHS = (
     "source/blender/editors",
     "source/blender/gpu",
     "source/blender/python",
+    "tests",
 )
 
 SOURCE_DIR = os.path.normpath(os.path.abspath(os.path.normpath(
     os.path.join(os.path.dirname(__file__), "..", "..", ".."))))
 
-PATHS = tuple(os.path.join(SOURCE_DIR, p) for p in PATHS)
+PATHS = tuple(
+    os.path.join(SOURCE_DIR, p.replace("/", os.sep))
+    for p in PATHS
+)
